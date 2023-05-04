3e1d219bbd76abf4d0d7eeeeb0ca4a25848780fe
delrizzo <Dave.DelRizzo@nrc-cnrc.gc.ca>
2023-05-04 11:59:39 -0700

    refactored spfrx-spectrum-plotter

--- images/ska-mid-dish-spfrx-talondx-console-plot/spfrx-spectrum-plotter.py
+++ images/ska-mid-dish-spfrx-talondx-console-plot/spfrx-spectrum-plotter.py
@@ -1,20 +1,36 @@
+#!/usr/bin/env python3
+
 import argparse
+import getpass
+import logging
 
 import matplotlib
 import matplotlib.animation as anim
 import matplotlib.pyplot as plt
 import numpy as np
-import pytango_client_wrapper as pcw
 
-# import os
+from pytango_client_wrapper import PyTangoClientWrapper
 
+# matplotlib.use("TkAgg")
 
-matplotlib.use("TkAgg")
+LOG_FORMAT = (
+    "[spfrx-spectrum-plotter.py: line %(lineno)s]%(levelname)s: %(message)s"
+)
+VERSION = "0.0.1"
+
+SPFRX_DEVICE = "psi-spfrx"
+SPFRX_NAME = "rxpu"
+SPFRX_CTRL_ALIAS = "controller"
+SPFRX_PKTCAP_ALIAS = "pktcap"
+
+DEFAULT_THROTTLE_INTERVAL = 100
+DEFAULT_NUM_PACKETS = 50
+DEFAULT_UPDATE_INTERVAL = 1000
 
 
 class SpectrumPlotter:
 
-    _test_mode = False
+    _test_mode: bool
     _raw = []
 
     _ax1 = None
@@ -28,30 +44,112 @@ class SpectrumPlotter:
     _data_xy_re = []
     _data_xy_im = []
 
-    def __init__(self):
-        self._args = self.parseInputArguments()
+    _device = SPFRX_DEVICE
+    _name = SPFRX_NAME
+    _ctrl = SPFRX_CTRL_ALIAS
+    _pktcap = SPFRX_PKTCAP_ALIAS
 
-        self._test_mode = self._args.test_mode
+    _throttle_interval: int
+    _n_packets: int
+    _update_interval: int
+    _mag: bool
 
-        self.proxyInit()
+    def __init__(self,
+                 throttle_interval: int,
+                 n_packets: int,
+                 update_interval: int,
+                 mag: bool,
+                 device: str = SPFRX_DEVICE,
+                 name: str = SPFRX_NAME,
+                 ctrl: str = SPFRX_CTRL_ALIAS,
+                 pktcap: str = SPFRX_PKTCAP_ALIAS,
+                 test_mode: bool = False) -> None:
+        """
+        Initialize the plot object
+
+        :param trottle_interval: The throttle interval for the Gated
+                                 Spectrometer expressed in ms.
+        :param n_packets: The number of packets to capture in each interval.
+        :param update_interval: The interval in ms in which to read the
+                                spectrum and reset the Gated Spectrometer
+        :param mag: A boolean to indicate display of cross product magnitude
+                    is requested rather than read/imaginary
+        :param device: (Optional) Override the device part of FQDN
+        :param name: (Optional) Override the name part of FQDN
+        :param ctrl: (Optional) Override the alias part of controller device
+        :param pktcap: (Optional) Override the alias part of pktcap device
+        :param test_mode: (Optional) Run in test mode if True.
+                          Default is False.
+        """
+
+        self._device = device
+        self._name = name
+        self._ctrl = ctrl
+        self._pktcap = pktcap
+
+        self._throttle_interval = throttle_interval
+        self._n_packets = n_packets
+        self._update_interval = update_interval
+        self._mag = mag
+
+        self._test_mode = test_mode
+
+        logger_.info(
+            "SPFRx Gated Spectrometer Plotter parameters:\n"
+            f"  SPFRx controller device: {self.getFqdn(self._ctrl)}\n"
+            f"  SPFRx packet cap device: {self.getFqdn(self._pktcap)}\n"
+            f"  Throttle Interval: {self._throttle_interval} ms\n"
+            f"  Number of Packets: {self._n_packets} packets\n"
+            f"  Update interval: {self._update_interval} ms\n"
+            f"  Plot type: {'Magnitudes' if self._mag else 'Real/Imaginary'}\n"
+            f"  Test mode: {'ENGAGED' if self._test_mode else 'NOT ACTIVE'}"
+            )
+
         self.plotInit()
 
-    def donothing(self):
-        pass
+    def getFqdn(self, alias: str) -> str:
+        """
+        Construct the TANGO FQDN from supplied string values in the
+        following form:
 
-    def proxyInit(self):
-        self._pcap = self.clientSetUp(self._args.device_name)
-        self._ctrl = self.clientSetUp(self._args.controller_name)
+        device/name/alias
 
-    def clientSetUp(self, device_name):
+        :param alias: Alias string
+        :param device: Device string, defaults to SPFRX_DEVICE
+        :param name: Name string, defaults to SPFRX_NAME
+        :returns: A TANGO FQDN string
+        """
+        return f"{self._device}/{self._name}/{alias}"
 
-        # Create the tango client
-        proxy = pcw.PyTangoClientWrapper()
-        proxy.create_tango_client(device_name)
+    def plotInit(self):
+        """
+        Initialize the plot object
+        """
 
-        return proxy
+        logger_.info("Initializing the Gated Spectrometer Plotter")
+        spfrx_pktcap = PyTangoClientWrapper()
+        spfrx_pktcap.create_tango_client(self.getFqdn(self._pktcap))
+        spfrx_pktcap.set_timeout_millis(5000)
 
-    def createPlot(self):
+        if not self._test_mode:
+            spfrx_pktcap.write_attribute(
+                "spectrometer_throttle_interval",
+                self._throttle_interval
+            )
+            spfrx_pktcap.write_attribute(
+                "spectrometer_num_packets",
+                self._n_packets
+            )
+
+        fig = self.createPlot()
+
+        anim.FuncAnimation(fig, self.update, frames=1, repeat=True)
+        plt.show()
+
+    def createPlot(self) -> plt.Figure:
+        """
+        Create the matplotlib Figure object
+        """
 
         fig = plt.figure(figsize=(16, 6))
         self._ax1 = fig.add_subplot(141)
@@ -65,22 +163,18 @@ class SpectrumPlotter:
 
         return fig
 
-    def plotInit(self):
+    def update(self) -> None:
+        """
+        Update the data within the plot.
+        """
 
-        if not self._test_mode:
-            self._pcap.write_attribute(
-                "spectrometer_throttle_interval", self._args.throttle_interval
-            )
-            self._pcap.write_attribute(
-                "spectrometer_num_packets", self._args.packets
-            )
+        spfrx_ctrl = PyTangoClientWrapper()
+        spfrx_ctrl.create_tango_client(self.getFqdn(self._ctrl))
+        spfrx_ctrl.set_timeout_millis(5000)
 
-        fig = self.createPlot()
-
-        anim.FuncAnimation(fig, self.update, frames=1, repeat=True)
-        plt.show()
-
-    def update(self, i):
+        spfrx_pktcap = PyTangoClientWrapper()
+        spfrx_pktcap.create_tango_client(self.getFqdn(self._pktcap))
+        spfrx_pktcap.set_timeout_millis(5000)
 
         attH = ""
         attV = ""
@@ -88,12 +182,19 @@ class SpectrumPlotter:
         if self._test_mode:
             self._raw = range(8202)
         else:
-            self._pcap.command_read_write("spectrometer_retrieve_result", None)
-            self._raw = self._pcap.read_attribute(
+            spfrx_pktcap.command_read_write(
+                "spectrometer_retrieve_result",
+                None
+            )
+            self._raw = spfrx_pktcap.read_attribute(
                 "spectrometer_spectrum_result"
             )
-            attH = self._ctrl.read_attribute("attenuationPolH")
-            attV = self._ctrl.read_attribute("attenuationPolV")
+            attH = spfrx_ctrl.read_attribute(
+                "attenuationPolH"
+            )
+            attV = spfrx_ctrl.read_attribute(
+                "attenuationPolV"
+            )
             if attH is None:
                 attH = -1
             if attV is None:
@@ -103,11 +204,15 @@ class SpectrumPlotter:
         self.updatePlot(timestamp, attH, attV)
 
         if timestamp % 10 == 0:
-            self._ctrl.command_noargs("MonitorPing")
-        plt.pause(self._args.update_interval / 1000)
+            spfrx_ctrl.command_read_write("MonitorPing")
+        plt.pause(self._update_interval / 1000)
 
-    def parseData(self):
+    def parseData(self) -> int:
+        """
+        Parse the raw spectrometer data.
 
+        :returns: An integer timestamp
+        """
         timestamp = self._raw[0]  # | (raw_data[1] << 16)
 
         self._data_xx = np.array([self._raw[2:1027], self._raw[4102:5127]])
@@ -130,12 +235,22 @@ class SpectrumPlotter:
 
         return timestamp
 
-    def updatePlot(self, timestamp, attH=-1, attV=-1):
+    def updatePlot(self,
+                   timestamp: int,
+                   attH: float = -1,
+                   attV: float = -1) -> None:
+        """
+        Update the plot figure object.
+        """
 
-        mag = self._args.mag
+        spfrx_ctrl = PyTangoClientWrapper()
+        spfrx_ctrl.create_tango_client(self.getFqdn(self._ctrl))
+        spfrx_ctrl.set_timeout_millis(5000)
 
-        kvalue = self._ctrl.read_attribute("kValue")
-        band = self._ctrl.read_attribute("configuredBand")
+        mag = self._mag
+
+        kvalue = spfrx_ctrl.read_attribute("kValue")
+        band = spfrx_ctrl.read_attribute("configuredBand")
 
         if band == 3:
             range_low = (3.168e9 + (kvalue * 1440)) / 2 / 1e6
@@ -287,83 +402,112 @@ class SpectrumPlotter:
             )
         plt.show()
 
-    def parseInputArguments(self):
-        """
-        parseInputArguments handles the processing of all command
-        line input arguments.
-        """
-        # thispath = os.path.abspath(os.path.dirname(__file__))  # this file.
-        parser = argparse.ArgumentParser()
-        #
-        # Optional Arguments
-        parser.add_argument(
-            "-d",
-            "--device_name",
-            type=str,
-            required=True,
-            help="Provide the TANGO device name of the ska-mid-spfrx-packet"
-            + "-capture-ds device server.",
-        )
-        parser.add_argument(
-            "-c",
-            "--controller_name",
-            type=str,
-            required=True,
-            help="Provide the TANGO device name of the ska-mid-spfrx-"
-            + "controller-ds device server.",
-        )
-        parser.add_argument(
-            "-t",
-            "--throttle_interval",
-            type=int,
-            required=True,
-            help="Provide the throttle interval in milliseconds for the "
-            + "Gated Spectrometer.",
-        )
-        parser.add_argument(
-            "-p",
-            "--packets",
-            type=int,
-            required=True,
-            help="Provide the number of packets to capture every "
-            + "throttle interval.",
-        )
-        parser.add_argument(
-            "-u",
-            "--update_interval",
-            type=int,
-            default=1000,
-            help="Provide the interval in milliseconds to read the spectrum "
-            + "result attribute and reset the Gated Spectrometer.",
-        )
-        parser.add_argument(
-            "-tm",
-            "--test_mode",
-            action="store_true",
-            help="Run the spectrometer gui in 'test' mode",
-        )
-        parser.add_argument(
-            "-b",
-            "--band",
-            type=int,
-            default=1,
-            help="Provide the band number to be plotted [1,2,3]",
-        )
-        parser.add_argument(
-            "-m",
-            "--mag",
-            action="store_true",
-            help="Use this argument to display cross products as magnitude "
-            + "& phase. Default is to display cross power of real/imaginary",
-        )
-
-        return parser.parse_args()
-
-
-def main():
-    sp = SpectrumPlotter()
-    sp.donothing()
-
 
 if __name__ == "__main__":
-    main()
+    logging.basicConfig(format=LOG_FORMAT, level=logging.INFO)
+    logger_ = logging.getLogger("spfrx-talondx.py")
+    logger_.info(f"User: {getpass.getuser()}")
+
+    parser = argparse.ArgumentParser()
+    #
+    parser.add_argument(
+        "-t",
+        "--throttle_interval",
+        type=int,
+        metavar="THROTTLE_INTERVAL",
+        default=DEFAULT_THROTTLE_INTERVAL,
+        help="Provide the throttle interval in milliseconds for the "
+        + f"Gated Spectrometer (Default is {DEFAULT_THROTTLE_INTERVAL} ms)",
+    )
+    parser.add_argument(
+        "-p",
+        "--packets",
+        type=int,
+        metavar="N_PACKETS",
+        default=DEFAULT_NUM_PACKETS,
+        help="Provide the number of packets to capture every "
+        + f"throttle interval (Default is {DEFAULT_NUM_PACKETS} packets)",
+    )
+    # Optional Arguments
+    parser.add_argument(
+        "-d",
+        "--device",
+        type=str,
+        metavar="DEVICE",
+        default=SPFRX_DEVICE,
+        help=f"Override the default FQDN device (Default is '{SPFRX_DEVICE}')"
+    )
+    parser.add_argument(
+        "-n",
+        "--name",
+        type=str,
+        metavar="NAME",
+        default=SPFRX_NAME,
+        help=f"Override the default FQDN name (Default is '{SPFRX_NAME}')"
+    )
+    parser.add_argument(
+        "-ctrl",
+        "--controller_name",
+        type=str,
+        metavar="ALIAS",
+        default=SPFRX_CTRL_ALIAS,
+        help="Override the default FQDN alias for the controller device. "
+             f"(Default is '{SPFRX_CTRL_ALIAS}')",
+    )
+    parser.add_argument(
+        "-pktcap",
+        "--packet_capture_name",
+        type=str,
+        metavar="ALIAS",
+        default=SPFRX_PKTCAP_ALIAS,
+        help="Override the default FQDN alias for the packet capture device. "
+             f"(Default is '{SPFRX_PKTCAP_ALIAS}')",
+    )
+    parser.add_argument(
+        "-u",
+        "--update_interval",
+        type=int,
+        metavar="UPDATE_INTERVAL",
+        default=DEFAULT_UPDATE_INTERVAL,
+        help="Provide the interval in milliseconds to read the spectrum "
+             "result attribute and reset the Gated Spectrometer "
+             f"(Default is {DEFAULT_UPDATE_INTERVAL} ms)"
+    )
+    parser.add_argument(
+        "-tm",
+        "--test_mode",
+        action="store_true",
+        help="Run the spectrometer gui in 'test' mode",
+    )
+    parser.add_argument(
+        "-m",
+        "--mag",
+        action="store_true",
+        help="Use this argument to display cross products as magnitude "
+        + "& phase (Default is to display cross power as real/imaginary)",
+    )
+    parser.add_argument(
+        "-v",
+        "--plotter_version",
+        action="store_true",
+        help="Print the plotter version",
+    )
+    args = parser.parse_args()
+
+    if args.plotter_version:
+        logger_.info(f"spfrx-spectrum-plotter VERSION:{VERSION}")
+        exit(0)
+
+    print(args)
+
+    sp = SpectrumPlotter(
+        args.throttle_interval,
+        args.packets,
+        args.update_interval,
+        args.mag,
+        args.device,
+        args.name,
+        args.controller_name,
+        args.packet_capture_name,
+        args.test_mode
+    )
